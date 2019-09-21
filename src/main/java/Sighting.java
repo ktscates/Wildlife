1634048bc86c45a4bf349066ac5d2b2cba881ae1
ktscates <cates2506@gmail.com>
2019-09-21 13:21:12 +0200

    Modify the sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,46 @@
+import org.sql2o.*;
+import java.util.List;
+
 public class Sighting {
+
+    private String ranger;
+    private String location;
+    private String date;
+
+    public Sighting(String ranger, String location, String date){
+        this.ranger = ranger;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getRanger(){
+        return ranger;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings(ranger, location, date) VALUES(:ranger, :location, :date)";
+            con.createQuery(sql)
+                    .addParameter("ranger", this.ranger)
+                    .addParameter("location", this.location)
+                    .addParameter("date", this.date)
+                    .executeUpdate();
+        }
+    }
+
+    public static List<Sighting> all(){
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Sighting.class);
+        }
+    }
 }
