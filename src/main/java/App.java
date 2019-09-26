386043c995241ceaf1aed009e96dcb6a6bfb7353
ktscates <cates2506@gmail.com>
2019-09-26 12:43:21 +0200

    heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,15 +10,6 @@ public class App {
 
     public static void main(String[] args){
 
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-        setPort(port);
-
         Map<String, Object> model = new HashMap<String, Object>();
         String layout = "templates/layout.hbs";
 
