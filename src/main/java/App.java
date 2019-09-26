9f3a3863f5dc8a07b218df1ccfa39ec5d39a52ac
ktscates <cates2506@gmail.com>
2019-09-26 11:40:37 +0200

    Add deployement port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,20 @@ public class App {
 
     public static void main(String[] args){
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         Map<String, Object> model = new HashMap<String, Object>();
         String layout = "templates/layout.hbs";
 
