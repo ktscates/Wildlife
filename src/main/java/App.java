8b5c1d3e59bcb464008cd0adf274d27a0c41b177
ktscates <cates2506@gmail.com>
2019-09-27 10:24:46 +0200

    Add pom.xml and modify app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,22 +7,18 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
     public static void main(String[] args){
 
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-
-        // This tells our app that if Heroku sets a port for us, we need to use that port.
-        // Otherwise, if they do not, continue using port 4567.
-
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-
-        port(port);
+            port(getHerokuAssignedPort());
+            staticFileLocation("/public");
 
         Map<String, Object> model = new HashMap<String, Object>();
         String layout = "templates/layout.hbs";
