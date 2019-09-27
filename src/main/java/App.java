ddeaea81357afa5c489c8a71f2b30ae599f65166
ktscates <cates2506@gmail.com>
2019-09-27 12:02:58 +0200

    Minor changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-import spark.template.handlebars.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import static spark.Spark.*;
 
